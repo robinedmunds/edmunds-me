935968b874611ef862d7e701968b9129fa80e1da
Robin Edmunds <r.edmunds@live.co.uk>
2020-04-15 16:13:33 +0100

    Move files and redo styles mobile first

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,7 +1,7 @@
 import "../styles/reset.css"
 import "../styles/main.scss"
 import "../styles/header.scss"
-import "../styles/project-card.scss"
+// import "../styles/project-card.scss"
 
 
 console.log('hello world')
