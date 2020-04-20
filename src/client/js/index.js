2df9dd3f78e50acc16baea4d1d135ef20e1e7d1a
Robin Edmunds <r.edmunds@live.co.uk>
2020-04-20 16:17:52 +0100

    Add blog.scss styles

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -2,6 +2,7 @@ import "../styles/reset.css"
 import "../styles/main.scss"
 import "../styles/header.scss"
 import "../styles/project-card.scss"
+import "../styles/blog.scss"
 
 
 console.log('hello world')
