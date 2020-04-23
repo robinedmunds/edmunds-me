60c6130afbc282073dedd67fa1190a1223357b38
Robin Edmunds <r.edmunds@live.co.uk>
2020-04-23 22:42:48 +0100

    Configure prismjs

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,10 +1,10 @@
+// styles
 import "../styles/reset.css"
 import "../styles/main.scss"
 import "../styles/header.scss"
 import "../styles/project-card.scss"
 import "../styles/blog.scss"
-import "prismjs/themes/prism.css"
-// import "prismjs/themes/prism-dark.css"
 
+// js
 import { Prism } from "prismjs"
-import "../js/html-toggle"
+// import "../js/html-toggle"
