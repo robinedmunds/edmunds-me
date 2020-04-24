4a25becb4490c6e92d40c3a30431da3d322ea4f7
Robin Edmunds <r.edmunds@live.co.uk>
2020-04-24 17:01:37 +0100

    Add fontawesome to build index

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,11 +1,12 @@
+// js
+import { Prism } from "prismjs"
+import "../js/html-toggle"
+
 // styles
 import "../styles/reset.css"
+import "@fortawesome/fontawesome-free/css/all.css"
 import "../styles/main.scss"
 import "../styles/header.scss"
 import "../styles/project-card.scss"
 import "../styles/blog.scss"
-
-// js
-import { Prism } from "prismjs"
-// import "../js/html-toggle"
 import "../styles/prismjs-custom.scss"
