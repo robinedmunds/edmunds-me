95eceac036d8444640f1f2ed89aaf7ab7b238a72
Robin Edmunds <r.edmunds@live.co.uk>
2020-04-28 20:55:14 +0100

    Refactor themeNext func in theme-select.js

--- src/client/js/theme-select.js
+++ src/client/js/theme-select.js
@@ -1,32 +1,29 @@
 "use strict"
 
 
-const themes = [
+const THEMES = [
   "theme-default",
   "theme-teal-fleece",
   "theme-orange-brown"
 ]
 
 const themeSelect = (name) => {
-  if (themes.includes(name)) {
+  if (THEMES.includes(name)) {
     const e = document.querySelector("html")
     e.classList = []
     e.classList.add(name)
-    console.log(`-- Theme set to: ${name}`)
+    // console.log(`-- Theme set to: ${name}`)
   }
 }
 
 const themeNext = () => {
   const e = document.querySelector("html")
-  const current = e.classList[0]
-  let idx = themes.indexOf(current)
-  if (idx < themes.length-1) {
-    idx++
-  } else {
-    idx = 0
-  }
-  themeSelect(themes[idx])
+  const currentTheme = e.classList[0]
+  let themeIndex = THEMES.indexOf(currentTheme)
+  if (themeIndex >= THEMES.length-1) {themeIndex = 0}
+  else {themeIndex++}
+  themeSelect(THEMES[themeIndex])
 }
 
-themeSelect(themes[0])
-window.setInterval(themeNext, 9000)
+themeSelect(THEMES[0])
+window.setInterval(themeNext, 3000)
