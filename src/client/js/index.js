fb9c433a1def67ddb57591bd60727837e1b3bb2d
Robin Edmunds <r.edmunds@live.co.uk>
2020-04-30 14:37:29 +0100

    Move style imports to main.scss

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -4,10 +4,5 @@ import "../js/html-toggle"
 import "../js/theme-select"
 
 // styles
-import "../styles/reset.css"
 import "@fortawesome/fontawesome-free/css/all.css"
 import "../styles/main.scss"
-import "../styles/header.scss"
-import "../styles/project-card.scss"
-import "../styles/blog.scss"
-import "../styles/prismjs-custom.scss"
